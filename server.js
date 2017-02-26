c5105a79a61e45c26a570e8b61e30b05ba84060b
HARIOM SHARMA <HARIOM SHARMA>
2017-02-26 18:09:51 +0530

    Folders

--- server.js
+++ server.js
@@ -5,24 +5,26 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.use(express.static('public'));
+app.use('/ui',express.static(__dirname+'/ui'));
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/ui/style.css', function (req, res) {
+/*
+app.get('/style', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
+});*/
+/*
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+});*/
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
+app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-
-
 
 app.get('/ui/dev.png',function(req,res){ res.sendFile(path.join(__dirname, 'ui','dev.png'));});
